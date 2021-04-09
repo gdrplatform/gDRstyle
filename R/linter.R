7e308affedf0f5b3ba56ca7590c1bcbf05074984
Dariusz Scigocki <dariusz.scigocki@contractors.roche.com>
2021-04-09 02:12:16 +0200

    added lintPkgInDir

--- R/linter.R
+++ R/linter.R
@@ -1,6 +1,12 @@
 #' @export
 lintPkg <- function(pkg_name) {
-  flint <- getFilesToLint(pkg_name)
+  flint <- getFilesToLint(pkg_name = pkg_name)
+  lintAllFiles(flint)
+}
+
+#' @export
+lintPkgInDir <- function(pkg_dir) {
+  flint <- getFilesToLint(dir = pkg_dir)
   lintAllFiles(flint)
 }
 
@@ -8,12 +14,26 @@ lintPkg <- function(pkg_name) {
 # Internal
 #############
 
-getFilesToLint <- function(pkg_name) {
-  flint <- c(
-    dir(system.file("R", package = pkg_name), full.names = TRUE),
-    dir(system.file("tests", package = pkg_name), full.names = TRUE, recursive = TRUE),
-    dir(system.file("shiny", package = pkg_name), full.names = TRUE, recursive = FALSE, pattern = "*.R")
-  )
+getFilesToLint <- function(pkg_name = NULL, dir = NULL) {
+  
+  if (is.null(pkg_name) && is.null(dir)) {
+    stop("define one of arguments: 'pkg_name' or 'dir'")
+  } else if (!is.null(pkg_name)) {
+    if (!is.null(dir)){
+      warning("both 'pkg_name' or 'dir' are defined, only 'will be use'")
+    }
+    flint <- c(
+      dir(system.file("R", package = pkg_name), full.names = TRUE),
+      dir(system.file("tests", package = pkg_name), full.names = TRUE, recursive = TRUE),
+      dir(system.file("shiny", package = pkg_name), full.names = TRUE, recursive = FALSE, pattern = "*.R")
+    )
+  } else {
+    flint <- c(
+      dir(file.path(dir, "R"), full.names = TRUE),
+      dir(file.path(dir, "tests"), full.names = TRUE, recursive = TRUE),
+      dir(file.path(dir, "shiny"), full.names = TRUE, recursive = FALSE, pattern = "*.R")
+    )
+  }
 
   # ignore extensions
   ignore.ext <- c("md", "html")
