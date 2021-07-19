2951ff47d9aab71b8d9dfe51ad04592060691c36
allisonvuong <allison.vuong@gmail.com>
2021-07-19 09:26:38 -0700

    test: try moving tst packages to subfolder

--- tests/testthat/test-linter.R
+++ tests/testthat/test-linter.R
@@ -3,6 +3,6 @@ library(gDRstyle)
 
 test_that("linting functions work as expected", {
   # Bad.
-  expect_error(lintPkg("dummy_pkg_errors"), regex = "Found lints")
-  expect_error(lintPkgDirs("dummy_pkg_errors"), regex = "*test.R")
+  expect_error(lintPkg(file.path("tst_pkgs", "dummy_pkg_errors")), regex = "Found lints")
+  expect_error(lintPkgDirs(file.path("tst_pkgs", "dummy_pkg_errors")), regex = "*test.R")
 })
---
+++ tests/testthat/tst_pkgs/dummy_pkg/R/test.R
@@ -0,0 +1,4 @@
+#' @keywords internal
+tstCharLimitLintr <- function() {
+  print("This file is simply a test to see whether or not the lintr functions appropriately pass tests.")
+}
