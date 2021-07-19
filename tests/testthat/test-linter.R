6080be01deaa4c0afe4fa835783255e1e9381554
allisonvuong <allison.vuong@gmail.com>
2021-07-19 09:05:44 -0700

    only use files in testthat folder

--- tests/testthat/test-linter.R
+++ tests/testthat/test-linter.R
@@ -2,7 +2,7 @@ library(testthat)
 library(gDRstyle)
 
 test_that("linting functions work as expected", {
-  devtools::wd()
-  expect_error(lintPkg(), regex = "Found lints")
-  expect_error(lintPkgDirs(), regex = "*test.R")
+  # Bad.
+  expect_error(lintPkg(testthat::test_path("dummy_pkg_errors")), regex = "Found lints")
+  expect_error(lintPkgDirs(testthat::test_path("dummy_pkg_errors")), regex = "*test.R")
 })
