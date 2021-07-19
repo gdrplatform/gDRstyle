48c9f358275d1d9609e8b501461605338e33b463
allisonvuong <allison.vuong@gmail.com>
2021-07-19 13:25:47 -0700

    test: negative control for requirement of DESCRIPTION file

--- tests/testthat/test-linter.R
+++ tests/testthat/test-linter.R
@@ -1,13 +1,11 @@
-library(testthat)
-library(gDRstyle)
-
 test_that("linting functions work as expected", {
   # Bad.
   pkg_path <- file.path("tst_pkgs", "dummy_pkg_errors")
-  print(getwd())
-  withr::with_dir(pkg_path, print(getwd()))
-  withr::with_dir(pkg_path, print(list.files()))
-  withr::with_dir(pkg_path, print(normalizePath(".")))
   expect_error(withr::with_dir(pkg_path, lintPkg(".")), regex = "Found lints")
   expect_error(withr::with_dir(pkg_path, lintPkgDirs(".")), regex = "*test.R")
+
+  # Good.
+  pkg_path <- file.path("tst_pkgs", "dummy_pkg")
+  expect_error(withr::with_dir(pkg_path, lintPkg(".")), NA)
+  expect_error(withr::with_dir(pkg_path, lintPkgDirs(".")), NA)
 })
