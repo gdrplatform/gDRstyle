7754a6adcf588c4e46be96dceb82ae1fcba2691a
allisonvuong <allison.vuong@gmail.com>
2021-07-19 12:37:30 -0700

    test: try CI again

--- tests/testthat/test-linter.R
+++ tests/testthat/test-linter.R
@@ -5,7 +5,8 @@ test_that("linting functions work as expected", {
   # Bad.
   pkg_path <- file.path("tst_pkgs", "dummy_pkg_errors")
   print(getwd())
-  expect_error(withr::with_dir(pkg_path), print(getwd()))
+  expect_error(withr::with_dir(pkg_path, print(getwd())))
+  expect_error(withr::with_dir(pkg_path, print(normalizePath(getwd()))))
   expect_error(withr::with_dir(pkg_path, lintPkg(".")), regex = "Found lints")
   expect_error(withr::with_dir(pkg_path, lintPkgDirs(".")), regex = "*test.R")
 })
