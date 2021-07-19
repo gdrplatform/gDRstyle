939e7a5cfa832e0fbda5766536d2b0318b26c293
allisonvuong <allison.vuong@gmail.com>
2021-07-19 09:16:23 -0700

    test: remove paths

--- tests/testthat/test-linter.R
+++ tests/testthat/test-linter.R
@@ -3,6 +3,6 @@ library(gDRstyle)
 
 test_that("linting functions work as expected", {
   # Bad.
-  expect_error(lintPkg(testthat::test_path("dummy_pkg_errors")), regex = "Found lints")
-  expect_error(lintPkgDirs(testthat::test_path("dummy_pkg_errors")), regex = "*test.R")
+  expect_error(lintPkg("dummy_pkg_errors"), regex = "Found lints")
+  expect_error(lintPkgDirs("dummy_pkg_errors"), regex = "*test.R")
 })
