5c5c8762e765b49cd09b4c2f00a5ae27001373a6
allisonvuong <allison.vuong@gmail.com>
2021-07-19 13:05:24 -0700

    test: more print statements debugging

--- tests/testthat/test-linter.R
+++ tests/testthat/test-linter.R
@@ -5,9 +5,9 @@ test_that("linting functions work as expected", {
   # Bad.
   pkg_path <- file.path("tst_pkgs", "dummy_pkg_errors")
   print(getwd())
-  expect_error(withr::with_dir(pkg_path, print(getwd())))
-  expect_error(withr::with_dir(pkg_path, list.files()))
-  expect_error(withr::with_dir(pkg_path, print(normalizePath(getwd()))))
+  withr::with_dir(pkg_path, print(getwd()))
+  withr::with_dir(pkg_path, print(list.files()))
+  withr::with_dir(pkg_path, print(normalizePath(".")))
   expect_error(withr::with_dir(pkg_path, lintPkg(".")), regex = "Found lints")
   expect_error(withr::with_dir(pkg_path, lintPkgDirs(".")), regex = "*test.R")
 })
