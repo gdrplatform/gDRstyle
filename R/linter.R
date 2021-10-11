df32e039ea141e2c398d457fd9d6a2ac32c6c0a6
Kamil Foltynski <kamil.foltynski@contractors.roche.com>
2021-10-11 22:28:47 +0000

    cleanup

--- R/linter.R
+++ R/linter.R
@@ -85,23 +85,3 @@ lintDir <- function(pkg_dir = ".", sub_dir) {
   return(invisible(NULL))
 }
 
-
-assignment_linter2 <- function() {
-  Linter(function(source_file) {
-    lapply(
-      ids_with_token(source_file, "EQ_ASSIGN"),
-      function(id) {
-        browser()
-        parsed <- with_id(source_file, id)
-        Lint(
-          filename = source_file$filename,
-          line_number = parsed$line1,
-          column_number = parsed$col1,
-          type = "style",
-          message = "Use <-, not =, for assignment.",
-          line = source_file$lines[as.character(parsed$line1)]
-        )
-      })
-  })
-}
-
