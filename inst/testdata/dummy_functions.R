4f69b68d92317b02b5453ddc95c9110eb820cad2
Kamil Foltynski <kamil.foltynski@contractors.roche.com>
2021-10-11 22:30:30 +0000

    added test data

---
+++ inst/testdata/dummy_functions.R
@@ -0,0 +1,33 @@
+#' dummy_fun1
+#'
+#' @return
+#' @export
+#'
+dummy_fun1 <- function() {
+  sapply(1:3, function(x) print(x))
+}
+
+#' dummy_fun2
+#'
+#' Dummy documented function
+#'
+#' @export
+#'
+#' @author Kamil Foltyński
+dummy_fun2 <- function() {
+  sapply(1:3, function(x) print(x))
+}
+
+#' dummy_fun3
+#'
+#' @return
+#' @export
+#'
+dummy_fun3 <- function() {
+  sapply(1:3, function(x) print(x))
+}
+
+#' @noRd
+dummy_fun4 <- function() {
+  sapply(1:3, function(x) print(x))
+}
