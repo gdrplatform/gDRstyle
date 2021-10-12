4ecd9cbff33a57e13022bf89bc0bec685b3c1464
Kamil Foltynski <kamil.foltynski@contractors.roche.com>
2021-10-12 09:27:10 +0000

    commented out, because will require a lot of work to make it work with all repos

--- R/linters_config.R
+++ R/linters_config.R
@@ -26,5 +26,5 @@ linters_config <- lintr::with_defaults(
   #assignment_linter = NULL,
   object_usage_linter = NULL,
   object_length_linter = NULL,
-  roxygen_tag_linter = roxygen_tag_linter()
+  # roxygen_tag_linter = roxygen_tag_linter() # commented out, because will require a lot of work to make it work with all repos
 )
