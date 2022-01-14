e4aed78e769774bd4e4f610752d5cfcd6002469c
allisonvuong <allison.vuong@gmail.com>
2022-01-14 15:43:53 -0800

    test: checkDependencies

---
+++ tests/testthat/test-dependencies.R
@@ -0,0 +1,18 @@
+library(testthat); library(gDRstyle)
+
+test_that("checkDependencies works as expected", {
+  expect_error(checkDependencies(system.file(package="gDRstyle", "testdata")),
+    regex="misaligned package versions between 'rplatform/dependencies.yaml' and package 'DESCRIPTION' file: B")
+})
+
+test_that("compare_versions works as expected", {
+  rp <- list("A"=">= 1.0.0", B=">= 1.2.1")
+  desc <- list("A"=">= 1.0.0", B=">=1.2.2")
+  expect_equal(gDRstyle:::compare_versions(rp, rp), NULL)
+  expect_equal(gDRstyle:::compare_versions(rp, desc), "B")
+  expect_error(gDRstyle:::compare_versions(rp, desc[1]))
+
+  rp2 <- list("A"=">= 1.0.0", C=">= 1.2.1")
+  desc2 <- list("A"=">= 1.0.0", C="*")
+  expect_equal(gDRstyle:::compare_versions(rp2, desc2), "C")
+})
