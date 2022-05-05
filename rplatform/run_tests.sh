8601b5a71894e524f6264d96d24f3b0bf739fa44
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2022-05-05 12:45:56 +0200

    build: use new logic for CI

---
+++ rplatform/run_tests.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+repo_path="$1"
+
+echo "Executing $0"
+echo "Environment: ${rp_env}"
+echo "Working directory: `pwd`"
+echo "Working directory contains: `ls | tr '\n' ' '`"
+
+# exit when any command fails
+set -e
+
+echo ">>>>>>>> Running linter"
+Rscript -e "gDRstyle::lintPkgDirs('/mnt/vol/gDRstyle')"
+
+echo ">>>>> RUNNING UNIT TESTS"
+Rscript -e "testthat::test_local(path = '$repo_path', stop_on_failure = TRUE)"
+
+echo ">>>>> RUNNING DEVTOOLS::CHECK()"
+sudo R CMD check --no-build-vignettes --no-manual --no-tests /mnt/vol/gDRstyle
+
