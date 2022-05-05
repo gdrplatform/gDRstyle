00fdb1471b1f613b52ecd968acb35697fba88fa8
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2022-05-05 14:27:16 +0200

    chore: clean-up tests

--- tests/testthat/test-linter.R
+++ tests/testthat/test-linter.R
@@ -1,11 +1,11 @@
 test_that("linting functions work as expected", {
   # Bad.
-  pkg_path <- file.path("tst_pkgs", "dummy_pkg_errors")
+  pkg_path <- system.file(package = "gDRstyle", "tst_pkgs", "dummy_pkg_errors")
   expect_error(withr::with_dir(pkg_path, lintPkg(".")), regex = "Found lints")
   expect_error(withr::with_dir(pkg_path, lintPkgDirs(".")), regex = "*test.R")
 
   # Good.
-  pkg_path <- file.path("tst_pkgs", "dummy_pkg")
+  pkg_path <- system.file(package = "gDRstyle", "tst_pkgs", "dummy_pkg")
   expect_error(withr::with_dir(pkg_path, lintPkg(".")), NA)
   expect_error(withr::with_dir(pkg_path, lintPkgDirs(".")), NA)
 })
