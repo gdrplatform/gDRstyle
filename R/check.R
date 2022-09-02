db9d8c2e85f2466933e175ec39ad9f7c8b9edd0c
Marcin Kamianowski <marcin.kamianowski@contractors.roche.com>
2022-09-02 15:44:35 +0200

    feat: move tests script

---
+++ R/check.R
@@ -0,0 +1,30 @@
+#' Check package
+#'
+#' @param pkgName String of package name.
+#' @param pkgDir String of path to package directory.
+#' @param repoDir String of path to repository directory.
+#'
+#' @export
+checkPackage <- function(pkgName, repoDir, subdir) {
+  pkgDir <- file.path(repoDir, subdir)
+
+  stopifnot(
+    dir.exists(repoDir),
+    dir.exists(pkgDir)
+  )
+
+  cat("Lint")
+  gDRstyle::lintPkgDirs(pkgDir)
+  
+  cat("Tests")
+  devtools::test(pkgDir, stop_on_failure = TRUE)
+  
+  cat("Check")
+  devtools::check(pkgDir, error_on = "error", args = c('--no-build-vignettes', '--no-examples', '--no-manual', '--no-tests'))
+  
+  cat("Deps")
+  gDRstyle::checkDependencies(
+    desc_path = paste0(repoDir, '/', pkgName, '/DESCRIPTION'), 
+    dep_path = paste0(repoDir, '/rplatform/dependencies.yaml')
+  )
+}
--- scripts/run_tests.R
+++ scripts/run_tests.R
@@ -1,24 +1,17 @@
-REPO_DIR <- commandArgs(trailingOnly = TRUE)[1]
-LIB_DIR <- commandArgs(trailingOnly = TRUE)[2]
-PKG_DIR <- file.path(REPO_DIR, "gDRstyle")
-stopifnot(
-  dir.exists(REPO_DIR),
-  dir.exists(LIB_DIR),
-  dir.exists(PKG_DIR)
-)
+PKG_NAME <- commandArgs(trailingOnly = TRUE)[1]
+REPO_DIR <- commandArgs(trailingOnly = TRUE)[2]
+PKG_SUBDIR <- commandArgs(trailingOnly = TRUE)[3]
+LIB_DIR <- commandArgs(trailingOnly = TRUE)[4]
 
 # Load libraries
-invisible(sapply(list.files(LIB_DIR, , pattern = "*.R$", full.names = TRUE), source, .GlobalEnv))
-
-# If the package uses shinytest, tests should be done outside of devtools::check (check with '--no-tests')
-cat("Lint")
-gDRstyle::lintPkgDirs(PKG_DIR)
-cat("Tests")
-devtools::test(PKG_DIR, stop_on_failure = TRUE)
-cat("Check")
-devtools::check(PKG_DIR, error_on = "error", args = c('--no-build-vignettes', '--no-examples', '--no-manual', '--no-tests'))
-cat("Deps")
-gDRstyle::checkDependencies(
-  desc_path = paste0(REPO_DIR, '/DESCRIPTION'), 
-  dep_path = paste0(REPO_DIR, '/rplatform/dependencies.yaml')
+stopifnot(dir.exists(LIB_DIR))
+invisible(
+  sapply(
+    list.files(LIB_DIR, , pattern = "*.R$", full.names = TRUE), 
+    source, 
+    .GlobalEnv
+  )
 )
+
+# Check package
+gDRstyle::checkPackage(PKG_NAME, REPO_DIR, as.logical(PKG_SUBDIR))
