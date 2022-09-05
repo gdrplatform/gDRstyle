03c460cc924b333668e071031f43df00999105f5
Marcin Kamianowski <marcin.kamianowski@contractors.roche.com>
2022-09-05 14:52:16 +0200

    fix: debug

--- inst/scripts/run_tests.R
+++ inst/scripts/run_tests.R
@@ -3,6 +3,11 @@ REPO_DIR <- commandArgs(trailingOnly = TRUE)[2]
 PKG_SUBDIR <- commandArgs(trailingOnly = TRUE)[3]
 LIB_DIR <- commandArgs(trailingOnly = TRUE)[4]
 
+print(paste("Name:", PKG_NAME))
+print(paste("Repo:", REPO_DIR))
+print(paste("Subdir:", PKG_SUBDIR))
+print(paste("Lib:", LIB_DIR))
+
 # Load libraries
 stopifnot(dir.exists(LIB_DIR))
 invisible(
