183364310f8cc0f229144b89837fd94abe849913
Dariusz Scigocki <dariusz.scigocki@contractors.roche.com>
2023-04-28 12:11:46 +0200

    added skip for test if test package is not removed

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -49,6 +49,7 @@ testthat::test_that("verify version properly", {
 })
 
 testthat::test_that("install local properly", {
+  skip_if(requireNamespace("fakePkg", quietly = TRUE), "skip, if there is instaled fakePkg")
   on.exit(remove.packages("fakePkg"))
   testthat::expect_error(
     utils::packageVersion("fakePkg"),
