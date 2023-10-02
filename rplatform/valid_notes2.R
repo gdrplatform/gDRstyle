d38d8bf774ce104f8dc3e4e2e27ddcd571efb760
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2023-10-02 04:35:46 -0700

    test: add note exception

--- rplatform/valid_notes2.R
+++ rplatform/valid_notes2.R
@@ -3,5 +3,10 @@ global_valid_notes_l <- list(
     length = 3,
     index_to_check = 2,
     text_to_check = "Unexported object imported by a ':::' call: ‘remotes:::version_satisfies_criteria’"
-  )
+  ),
+  list(
+    length = 5,
+    index_to_check = 2,
+    text_to_check = "installed size is"
+  ),
 )
